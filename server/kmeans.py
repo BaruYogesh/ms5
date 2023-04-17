e721eeda4f072384bdfcca0ab86aaad04155cbad
BaruYogesh <bhargav.yogesh@gmail.com>
2023-04-17 15:01:30 -0400

    added middleware, started sessions

--- server/kmeans.py
+++ server/kmeans.py
@@ -6,6 +6,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.pyplot as plt
 import io
 from fastapi import UploadFile
+from sklearn.decomposition import LatentDirichletAllocation
+
 
 def make_df(file: UploadFile) -> pd.DataFrame:
 
@@ -42,3 +44,39 @@ def cluster_count_analysis(df: pd.DataFrame):
     buf.seek(0)
 
     return buf
+
+def monthly_dist(num_clusters, df):
+
+    MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] # Months for display
+    for cluster in range(num_clusters):
+        month_data = [0 for element in range(12)] # Initialize array of 0s to count distribution of months
+        LDA_Featureset = []
+        for point in range(len(df)):
+            if(df["Cluster"][point] == cluster): # Lazy way of grabbing specific clusters 
+                LDA_Featureset.append(str(df['Feature Title'][point]) + ' : '+ str(df['Feature Description'][point])) # Get information for LDA per cluster
+                py_month = pd.Timestamp(df["Release Date"][point]).month - 1 # pandas has timestamp to extract month as int easily
+                if(isinstance(py_month, int)): # Any row that doesn't have month data should be excluded
+                    month_data[py_month] += 1
+    
+    print(f'Cluster: {cluster}')
+    plt.bar(MONTHS, month_data)
+    plt.show()
+
+    # Get the most common words in the clusters
+    cv = sklearn.feature_extraction.text.TfidfVectorizer(max_df=.4, min_df=.1)
+    lda_data = cv.fit_transform(LDA_Featureset).toarray()
+
+    lda = LatentDirichletAllocation(n_components = 2, doc_topic_prior=1)
+    lda.fit(lda_data)
+
+    vocab = cv.get_feature_names_out()
+    topic_words = {}
+    n_top_words = 5
+    for topic, comp in enumerate(lda.components_):
+        word_idx = np.argsort(comp)[::-1][:n_top_words]
+        topic_words[topic] = [vocab[i] for i in word_idx]
+
+    for topic, words in topic_words.items():
+        print(f'Top words for topic {topic}')
+        print(' %s' % ', '.join(words))
+    print('\n')
--- server/server.py
+++ server/server.py
@@ -1,13 +1,120 @@
-from fastapi import FastAPI, UploadFile
-from fastapi.responses import StreamingResponse 
+from fastapi import FastAPI, UploadFile, Depends, HTTPException
+from fastapi.responses import StreamingResponse, Response
+from pydantic import BaseModel as PydanticBaseModel
+
 import kmeans
+
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+from uuid import uuid4, UUID
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from pandas import DataFrame
+
+origins = [
+    "http://localhost"
+]
+
+class BaseModel(PydanticBaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+class SessionData(BaseModel):
+    df: DataFrame
+
+cookie_params = CookieParameters()
+
+# Uses UUID
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE",
+    cookie_params=cookie_params,
+)
+backend = InMemoryBackend[UUID, SessionData]()
+
+class BasicVerifier(SessionVerifier[UUID, SessionData]):
+    def __init__(
+        self,
+        *,
+        identifier: str,
+        auto_error: bool,
+        backend: InMemoryBackend[UUID, SessionData],
+        auth_http_exception: HTTPException,
+    ):
+        self._identifier = identifier
+        self._auto_error = auto_error
+        self._backend = backend
+        self._auth_http_exception = auth_http_exception
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def auto_error(self):
+        return self._auto_error
+
+    @property
+    def auth_http_exception(self):
+        return self._auth_http_exception
+
+    def verify_session(self, model: SessionData) -> bool:
+        """If the session exists, it is valid"""
+        return True
+
+verifier = BasicVerifier(
+    identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/create_session/{name}")
+async def create_session(name: str, response: Response):
+
+    session = uuid4()
+    data = SessionData(username=name)
+
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
+
+    return f"created session for {name}"
+
+
+@app.get("/whoami", dependencies=[Depends(cookie)])
+async def whoami(session_data: SessionData = Depends(verifier)):
+    return session_data
+
+
+@app.post("/delete_session")
+async def del_session(response: Response, session_id: UUID = Depends(cookie)):
+    await backend.delete(session_id)
+    cookie.delete_from_response(response)
+    return "deleted session"
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# @app.get('/create_df')
+# async def create_df()
 
 @app.post("/cluster_count_analysis/")
 async def create_upload_file(file: UploadFile):
@@ -15,4 +122,13 @@ async def create_upload_file(file: UploadFile):
     df = kmeans.make_df(file)
 
     buf = kmeans.cluster_count_analysis(df)
-    return StreamingResponse(buf, media_type="image/png")
+
+    session = uuid4()
+    data = SessionData(df=df)
+
+    await backend.create(session, data)
+    resp = StreamingResponse(buf, media_type="image/png")
+    cookie.attach_to_response(resp, session)
+
+    return resp
+
